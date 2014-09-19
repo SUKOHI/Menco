be82c45f10cbfd64bfad17e424380e70fc27bcc4
sukohi <eagle.fly.free0722@gmail.com>
2014-09-19 13:20:02 +0900

    Hidden tags added.

---
+++ src/Sukohi/Menco/Menco.php
@@ -0,0 +1,68 @@
+<?php namespace Sukohi\Menco;
+
+class Menco {
+	
+	public function get($params=array(), $data=array()) {
+
+		$id = (isset($params['id'])) ? $params['id'] : md5(uniqid(rand(),1));
+		$url = (isset($params['url'])) ? $params['url'] : '';
+		$method = (isset($params['method'])) ? $params['method'] : 'POST';
+		$label = (isset($params['label'])) ? $params['label'] : 'submit';
+		$class = (isset($params['class'])) ? ' class="'. $params['class'] .'"' : '';
+		$message = (isset($params['message'])) ? $params['message'] : 'Are you sure?';
+
+		$form_property = $this->property(array(
+			'id' => $id,
+			'action' => $url, 
+			'method' => $method, 
+			'onsubmit' => 'if(!confirm(\''. $message .'\')){return false}'
+		));
+		$hidden = $this->hiddenTag($data);
+		
+		return '<form'. $form_property .'>'. $hidden .'<button type="submit"'. $class .'>'. $label .'</button></form>';
+		
+	}
+	
+	private function property($properties) {
+		
+		$return = '';
+		
+		foreach ($properties as $key => $property) {
+			
+			$return .= ' '. $key .'="'. $property .'"';
+			
+		}
+		
+		return $return;
+		
+	}
+	
+	private function hiddenTag($data) {
+		
+		$return = '';
+		
+		foreach ($data as $name => $value) {
+			
+			$return .= '<input type="hidden" name="'. $name .'" value="'. $value .'">';
+			
+		}
+		
+		return $return;
+		
+	}
+	
+}
+
+/*** Example
+
+	echo Menco::get(array(
+		'id' => 'remove', 
+		'label' => 'remove', 
+		'url' => URL::to('/'), 
+		'class' => 'btn btn-danger', 
+		'message' => 'Are you sure?'
+	));
+
+	// All of the parameters are skippable.
+
+ ***/
