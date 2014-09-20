b75b0efa7da4ba44dda722c005dac55d91f6aa9d
sukohi <eagle.fly.free0722@gmail.com>
2014-09-20 18:36:10 +0900

    <button> changed to <a>.

--- src/Sukohi/Menco/Menco.php
+++ src/Sukohi/Menco/Menco.php
@@ -4,22 +4,22 @@ class Menco {
 	
 	public function get($params=array(), $data=array()) {
 
-		$id = (isset($params['id'])) ? $params['id'] : md5(uniqid(rand(),1));
+		$id = (isset($params['id'])) ? 'menco_'. $params['id'] : 'menco_'. md5(uniqid(rand(),1));
 		$url = (isset($params['url'])) ? $params['url'] : '';
 		$method = (isset($params['method'])) ? $params['method'] : 'POST';
 		$label = (isset($params['label'])) ? $params['label'] : 'submit';
-		$class = (isset($params['class'])) ? ' class="'. $params['class'] .'"' : '';
+		$class = (isset($params['class'])) ? ' class="'. $params['class'] .'" ' : '';
 		$message = (isset($params['message'])) ? $params['message'] : 'Are you sure?';
 
 		$form_property = $this->property(array(
 			'id' => $id,
 			'action' => $url, 
-			'method' => $method, 
-			'onsubmit' => 'if(!confirm(\''. $message .'\')){return false}'
+			'method' => $method
 		));
 		$hidden = $this->hiddenTag($data);
 		
-		return '<form'. $form_property .'>'. $hidden .'<button type="submit"'. $class .'>'. $label .'</button></form>';
+		return '<form'. $form_property .' style="display:none;">'. $hidden .'</form>'.
+					'<a href="#" '. $class .'onclick="if(confirm(\''. $message .'\')){ document.getElementById(\''. $id .'\').submit(); } return false;">'. $label .'</button>';
 		
 	}
 	
