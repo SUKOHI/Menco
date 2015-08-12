d8876c849bba481f4e31daf62d67718c3e8ed73f
sukohi <eagle.fly.free0722@gmail.com>
2015-08-11 18:49:06 -0700

    First Commit

---
+++ src/Sukohi/Menco/MencoServiceProvider.php
@@ -0,0 +1,44 @@
+<?php namespace Sukohi\Menco;
+
+use Illuminate\Support\ServiceProvider;
+
+class MencoServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Bootstrap the application events.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		$this->package('sukohi/menco');
+	}
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array();
+	}
+
+}
