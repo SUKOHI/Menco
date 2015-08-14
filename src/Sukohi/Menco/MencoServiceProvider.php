9a23c8ff23f1858913a36abaf40a969f60ccee28
sukohi <eagle.fly.free0722@gmail.com>
2015-08-13 21:08:19 -0700

    recovered

--- src/Sukohi/Menco/MencoServiceProvider.php
+++ src/Sukohi/Menco/MencoServiceProvider.php
@@ -28,7 +28,10 @@ class MencoServiceProvider extends ServiceProvider {
 	 */
 	public function register()
 	{
-		//
+		$this->app['menco'] = $this->app->share(function($app)
+		{
+			return new Menco;
+		});
 	}
 
 	/**
@@ -38,7 +41,7 @@ class MencoServiceProvider extends ServiceProvider {
 	 */
 	public function provides()
 	{
-		return array();
+		return array('menco');
 	}
 
 }
